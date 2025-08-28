806bc07c134f5df3bfda48de8be9504105cc93df
ayushgpt01 <ayushgpt043@gmail.com>
2025-08-28 10:49:59 +0530

    init project

---
+++ backend/src/main/java/com/shopper/WindowShopperApplication.java
@@ -0,0 +1,13 @@
+package com.shopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowShopperApplication.class, args);
+	}
+
+}
